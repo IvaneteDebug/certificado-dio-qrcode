31f1a4811434d395c38d688f219b2dfa6b38b275
Ivanete <ivanetevieira1000@gmail.com>
2024-05-07 12:03:16 -0300

    Primeiro commit: Adiciona código inicial

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using QRCoder;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string linkCertificado = "https://hermes.dio.me/certificates/Q1ARNOWY.pdf";
+
+        string mensagemCertificado = "Veja meu certificado de manipulação de dados no SQL: " + linkCertificado;
+
+      string mensagemAdicional = "Novo certificado de manipulação de dados no SQL disponível! Confira o certificado e fique à vontade para compartilhar.";
+
+        string mensagemFinal = mensagemCertificado + " " + mensagemAdicional;
+
+        QRCodeGenerator qrGenerator = new QRCodeGenerator();
+        QRCodeData qrCodeData = qrGenerator.CreateQrCode(mensagemFinal, QRCodeGenerator.ECCLevel.Q);
+
+        BitmapByteQRCode qrCodeBytes = new BitmapByteQRCode(qrCodeData);
+        byte[] qrCodeImageBytes = qrCodeBytes.GetGraphic(20); 
+
+        File.WriteAllBytes("qrcode_certificado.png", qrCodeImageBytes);
+
+        Console.WriteLine("QR Code criado com sucesso! Scanee para acessar o certificado e ver a mensagem.");
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# certificado-dio-qrcode
+
+Este projeto gera um QR Code que direciona para o certificado de manipulação de dados no SQL na plataforma da DIO (Digital Innovation One). Compartilhe facilmente seu certificado com recrutadores e sua rede, incentivando a transparência e a visibilidade das suas habilidades.
+
+## Como usar
+
+1. Clone este repositório.
+2. Abra o arquivo Program.cs e modifique os links conforme necessário.
+3. Execute o programa para gerar o QR Code.
+4. Compartilhe o QR Code gerado para que outras pessoas possam escaneá-lo e acessar o certificado.
+
+## Contribuição
+
+Se você tiver sugestões de melhorias ou encontrar algum problema, sinta-se à vontade para abrir uma issue ou enviar um pull request.
